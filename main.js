448137cca5e2db27369e175c47fdebc785e44b2c
Marijana Koceva <kocevam@yahoo.com>
2024-12-03 12:39:22 -0800

    021224

---
+++ main.js
@@ -0,0 +1,8 @@
+// Declaring variabkle with const 
+const breakpointsTest = document.querySelector('.breakpoints-test');
+console.log(breakpointsTest);
+// Declaring variable with let
+let welcomeStudent = document.getElementById('welcomeStudents');
+console.log(welcomeStudents.innerHTML + "From the Console");
+//Changing html with js
+setTimeout(function() {welcomeStudents.innerHTML = "Changing text from JS"}, 5000)
