90a5f70c665f69ee394138beab9aa1f98bf126db
Simona Maksimova <simonamaksimova@yahoo.com>
2025-01-13 18:45:34 +0100

    Simona homework

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Declaring vafiable with const
+//Declaring variable with const
 const breakpointsTest = document.querySelector('.breakpoints-test');
 console.log(breakpointsTest)
 //Declaring variable with let
@@ -6,3 +6,5 @@ let welcomeStudents = document.getElementById('welcomeStudents');
 console.log(welcomeStudents.innerHTML + "From the Console");
 //Changing html with js
 setTimeout(function() {welcomeStudents.innerHTML = "Changing text from JS"}, 5000)
+
+
