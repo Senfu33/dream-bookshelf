d0a501dfe61af47cc37f862fccd363c9190f77cf
Veronika Tasova <vtaseva1@gmail.com>
2025-01-22 18:27:23 +0100

    Veronika Homework Jira

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Declaring variable with const
 const breakpointsTest = document.querySelector('.breakpoints-test');
 console.log(breakpointsTest)
-//Declaring variable with let
+// Declaring variable with let
 let welcomeStudents = document.getElementById('welcomeStudents');
 console.log(welcomeStudents.innerHTML + "From the Console");
 //Changing html with js
@@ -12,3 +12,97 @@ window.onscroll = () => {
     sliderContainer.style.left = `${-window.scrollx}px`
 }
 
+
+// temp = document.querySelectorAll(".variant1");
+//         console.log(temp[0]);
+//         console.log(temp[1])
+
+// const var1 = variant1.scss
+
+// function myFunction() {
+//     var element = document.getElementById("img-1");
+//      element.style.var1
+//          }
+
+//     //  -----------------
+// const first = document.getElementById("1");
+// const second = document.getElementById("2");
+// const third = document.getElementById("3");
+// const fourth = document.getElementById("4");
+// const fifth = document.getElementById("5");
+
+// const sliderContainer = document.querySelector('.slider-container');
+// window.onscroll = () => {
+//     sliderContainer.style.left
+// }
+
+// first.addEventListener("click", () => {
+//   const thisbook = slide.clientWidth;
+//   slidesContainer.scrollLeft += thisbook;
+// });
+
+// function myFunction(p, b) {
+//   element.style.opacity = b;
+// }
+// first.addEventListener("click", document.getElementById("1-img")
+// => { first =
+// });
+
+
+// fourth.addEventListener("click", () => {
+//   fourth.ariaValueNow = document.getElementById("4-img")
+//   const thisbook =  document.getElementById("4-img");
+//   fourth.append = thisbook;
+//     console.log(thisbook);
+//   });
+
+//   fourth.addEventListener('click', showNextItem);
+// previousItem.addEventListener('click', showPreviousItem);
+
+
+
+// const items = document.querySelectorAll('img');
+// const itemCount = items.length;
+// const nextItem = document.querySelector('.next');
+// const previousItem = document.querySelector('.previous');
+// let count = 0;
+
+// function showNextItem() {
+//   items[count].classList.remove('active');
+
+//   if(count < itemCount - 1) {
+//     count++;
+//   } else {
+//     count = 0;
+//   }
+
+//   items[count].classList.add('active');
+//   console.log(count);
+// }
+
+// function showPreviousItem() {
+//   items[count].classList.remove('active');
+
+//   if(count > 0) {
+//     count--;
+//   } else {
+//     count = itemCount - 1;
+//   }
+
+//   items[count].classList.add('active');
+//   console.log(count);
+// }
+
+// function keyPress(e) {
+//   e = e || window.event;
+
+//   if (e.keyCode == '37') {
+//     showPreviousItem();
+//   } else if (e.keyCode == '39') {
+//     showNextItem();
+//   }
+// }
+
+// nextItem.addEventListener('click', showNextItem);
+// previousItem.addEventListener('click', showPreviousItem);
+// document.addEventListener('keydown', keyPress);
